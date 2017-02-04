2a4e188cb5f8634f30f67908594eacebcb734c58
Francisco Fuentes <txi.francisco@gmail.com>
2017-02-04 00:43:10 -0300

    4 operations done.

--- src/java/base/CountryResource.java
+++ src/java/base/CountryResource.java
@@ -9,8 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -28,21 +30,23 @@ public class CountryResource {
     CountryDAO cdao = new CountryDAO();
 
     List<Country> countries = cdao.readAll();
-    GenericEntity<List<Country>> entity = new GenericEntity<List<Country>>(countries) {};
+    GenericEntity<List<Country>> entity = new GenericEntity<List<Country>>(countries) {
+    };
 
     @GET
     @Consumes({"application/json"})
     @Produces({"application/json"})
     public Response read(@QueryParam("id") String id) {
         try {
-                Country c = cdao.read(id);
+            Country c = cdao.read(id);
 
-                if (c != null) {
-                    return Response.status(200).entity(c).build();
-                } else {
-                    return Response.status(400).entity(String.format("Error: No country with such ID in server.")).build();
-                }
+            if (c != null) {
+                return Response.status(200).entity(c).build();
+            } else {
+                return Response.status(400).entity(String.format("Error: No country with such ID in server.")).build();
+            }
         } catch (NumberFormatException nfe) {
+            // If no id is provided it will return a list of countries
             if (id.isEmpty()) {
                 return Response.status(200).entity(entity).build();
             }
@@ -59,4 +63,37 @@ public class CountryResource {
         return Response.status(400).entity(c).build();
 
     }
+
+    @PUT
+    @Consumes({"application/xml", "application/json"})
+    public Response update(Country c) {
+        if (cdao.read(c.getId()) != null) {
+            if (cdao.update(c)) {
+                return Response.status(200).entity(c).build();
+            } else {
+                return Response.status(400).entity("Error: No valid ID provided.").build();
+            }
+        } else {
+            return Response.status(400).entity("Error: ID doesn't exist.").build();
+        }
+    }
+
+    @DELETE
+    @Consumes({"application/xml", "application/json"})
+    public Response delete(@QueryParam("id") String id) {
+        try {
+            if (cdao.delete(id)) {
+                return Response.status(200).entity("Done.").build();
+            } else {
+                return Response.status(400).entity(String.format("Error: No country with such ID in server.")).build();
+            }
+        } catch (NumberFormatException nfe) {
+            if (id.isEmpty()) {
+                return Response.status(400).entity("Error: Insert an ID.").build();
+            } else {
+                return Response.status(400).entity("Error: No valid ID provided.").build();
+            }
+        }
+    }
+
 }
